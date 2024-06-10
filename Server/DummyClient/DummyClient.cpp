00b7ffe3e624724835c7bd10895cef22c79d9ca8
Chaewony <tcd1205@naver.com>
2024-06-11 03:07:06 +0900

    Initial Summit

---
+++ Server/DummyClient/DummyClient.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include <iostream>
+
+#include <WinSock2.h> // ms의 윈도우 소켓 라이브러리
+
+int main()
+{
+
+}
---
+++ Server/GameServer/GameServer.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+int main()
+{
+
+}
